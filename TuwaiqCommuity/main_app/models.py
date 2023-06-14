a36122a7455186ec18ee19cd8eea72351e6de50b
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-06-14 23:24:45 +0300

    Added Bootcamp, Profile models

---
+++ TuwaiqCommuity/main_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Bootcamp(models.Model):
+    CATEGORY_CHOICES = [
+           ("frontend", "Front-end Development"),
+           ("backend", "Back-end Development"),
+           ("full_stack", "Full-stack Development"),
+           ("UI", "UI/UX"),
+           ("artificial", "Artificial Intelligence"),
+           ("security", "Cyber Security"),         
+    ]
+    name=models.CharField(max_length=2000)
+    bootcamp_descripton=models.TextField(blank=True)
+    category=models.CharField(max_length=2000,choices=CATEGORY_CHOICES,default="Front-end Development")
+    is_active=models.BooleanField(default=False)
+    logo=models.ImageField(upload_to="images/",default="images/bootstrap.png")
+    
+   
