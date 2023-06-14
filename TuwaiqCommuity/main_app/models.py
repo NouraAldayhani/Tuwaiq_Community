20a00378335f6136dbfe565a4b13fee13477dc0c
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-06-15 00:21:10 +0300

    Added start/end date fields for bootcamp model

--- TuwaiqCommuity/main_app/models.py
+++ TuwaiqCommuity/main_app/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -15,6 +16,8 @@ class Bootcamp(models.Model):
     bootcamp_descripton=models.TextField(blank=True)
     category=models.CharField(max_length=2000,choices=CATEGORY_CHOICES,default="Front-end Development")
     is_active=models.BooleanField(default=False)
+    start_date=models.DateField(default=timezone.now)
+    end_date=models.DateField(default=timezone.now() + timezone.timedelta(days=30))
     logo=models.ImageField(upload_to="images/",default="images/bootstrap.png")
     
    
