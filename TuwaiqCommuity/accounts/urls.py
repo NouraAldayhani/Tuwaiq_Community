f23a18e64a0db21bec91ff43337cf914c1032ee9
Dalia <daliaasq@gmail.com>
2023-06-15 14:27:13 +0300

    Request approval page

--- TuwaiqCommuity/accounts/urls.py
+++ TuwaiqCommuity/accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name= "accounts"
 
 urlpatterns=[
-    
+    path('request/',views.request_page,name="request_page"),
 ]
--- TuwaiqCommuity/accounts/views.py
+++ TuwaiqCommuity/accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+
+def request_page(request : HttpRequest):
+    return render(request, "accounts/request.html")
