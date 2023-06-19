3bfc223d63164fd5ea3a6414070793287b92301a
Dalia <daliaasq@gmail.com>
2023-06-19 22:07:10 +0300

    Events page and upcoming events. authorizations have been added

--- TuwaiqCommuity/main_app/urls.py
+++ TuwaiqCommuity/main_app/urls.py
@@ -12,9 +12,11 @@ urlpatterns=[
     path('bootcamp/<bootcamp_id>/',views.bootcamp_page,name="bootcamp_page"),
     path('add/question/<bootcamp_id>/',views.add_question,name="add_question"),
 
+    path('events/',views.events,name="events"),
     path('bootcamp/event/<bootcamp_id>',views.bootcamp_event,name="bootcamp_event"),
     path('create/event/<bootcamp_id>',views.create_event,name="create_event"),
     path('event/details/<event_id>',views.event_details,name="event_details"),
+    path('update/event/<event_id>',views.update_event,name="update_event"),
     path('delete/event/<event_id>',views.delete_event,name="delete_event"),
 
     path('about/',views.about_page,name="about_page"),
